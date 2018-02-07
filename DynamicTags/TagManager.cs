f4234a614cc1f2b67fc9a78710dc2a3668fac08a
Brett Hacker <brhacker@microsoft.com>
2018-02-07 01:14:33 -0600

    cleanup

--- DynamicTags/TagManager.cs
+++ DynamicTags/TagManager.cs
@@ -7,14 +7,11 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Azure.WebJobs.Host;
 using Newtonsoft.Json;
 using System.Dynamic;
-//using System.Configuration;
 using Microsoft.CSharp;
 using DynamicTags.Models;
 using System.Collections.Generic;
 using System;
 using Newtonsoft.Json.Linq;
-//using TableStorage;
-using System.Threading.Tasks;
 
 namespace DynamicTags
 {
@@ -45,7 +42,6 @@ namespace DynamicTags
                     ResourceWriteSuccess evt = JsonConvert.DeserializeObject<ResourceWriteSuccess>(JsonConvert.SerializeObject(response.Data));
                     var logItem = ProcessEvent.Process(evt);
 
-                    //log.Info(String.Format("Processed subscription event with data:\r\n{0}", requestBody));
                     log.Info(String.Format("New event - log data:\r\n{0}", JsonConvert.SerializeObject(logItem)));
                 }
 
